f14668d8882a410a5b814937e9a8ce1b5e69e860
William Berry <76190311+wberry86@users.noreply.github.com>
2021-10-07 12:41:10 -0500

    added fight function

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,10 +1,39 @@
-function fight() {
-  window.alert("The fight has begun!");
-  var playerName = window.prompt("What is your Robot's name?");
-  console.log(playerName);
-  console.log("This logs a string, good for leaving yourself a message");
-  console.log(10 + 10);
-  console.log("Our robot's name is " + playerName);
+var playerName = window.prompt("What is your name?");
+var playerHealth = 100;
+var playerAttack = 10;
+
+console.log(playerName, playerAttack, playerHealth);
+
+var enemyName = "Robot of Doom";
+var enemyHealth = 50;
+var enemyAttack = 12;
+
+console.log(enemyName, enemyAttack, enemyHealth);
+
+var fight = function() {
+    window.alert("Welcome to Robot Wars!");
+
+    enemyHealth = enemyHealth - playerAttack;
+    console.log(playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining.");
+
+    // check enemys health
+    if (enemyHealth <= 0) {
+        window.alert(enemyName + " has died!");
+    }
+    else {
+        window.alert(enemyName + " still has " + enemyHealth + " health left.");
+    }
+
+    playerHealth = playerHealth - enemyAttack;
+    console.log(enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining.");
+
+    // check players health
+    if (playerHealth <= 0) {
+        window.alert(playerName + " has died!");
+    }
+    else {
+        window.alert(playerName + " still has " + playerHealth + " health left.");
+    }
 }
 
 fight();
