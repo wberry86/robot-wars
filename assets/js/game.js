6b1ab57be3ee203540c92f070075d1fe266a85bf
William Berry <76190311+wberry86@users.noreply.github.com>
2021-10-26 20:16:07 -0500

    fixed fight or skip bug

--- assets/js/game.js
+++ assets/js/game.js
@@ -5,32 +5,46 @@ var randomNumber = function (min, max) {
   return value;
 };
 
+var fightOrSkip = function() {
+  // ask player if they would like to fight or skip using this function
+  var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter FIGHT or SKIP to choose");
+
+
+  if (promptFight === "" || promptFight === null) {
+    window.alert("You need to provide a valid answer! Please try again.");
+    return fightOrSkip();
+  }
+
+  promptFight = promptFight.toLowerCase();
+
+  // if player picks "skip" confirm and then stop the loop
+  if (promptFight === "skip") {
+    // confirm player wants to skip
+    var confirmSkip = window.confirm("Are you sure you want to quit?");
+    if (confirmSkip) {
+      window.alert(playerInfo.name + " has decided to skip the fight. Goodbye!");
+      // subtract money from playermoney for skipping
+      playerInfo.money = Math.max(0, playerInfo.money - 10);
+
+      // return true if player wants to leave
+      return true;
+
+      shop();
+    }
+  }
+  return false;
+}
+
 var fight = function (enemy) {
   console.log(enemy);
   // repeat and execute as long as the enemy robot is still alive
   while (enemy.health > 0 && playerInfo.health > 0) {
     // window.alert("Welcome to Robot Wars!");
 
-    var promptFight = window.prompt(
-      "Would you like to FIGHT or SKIP this battle? Enter FIGHT or SKIP to choose"
-    );
-    // console.log(promptFight);
-    if (
-      promptFight === "skip" ||
-      promptFight === "SKIP" ||
-      promptFight === "Skip"
-    ) {
-      var confirmSkip = window.confirm("Are you sure you want to quit?");
-      if (confirmSkip) {
-        window.alert(
-          playerInfo.name + " has chosen to skip the fight. Goodbye!"
-        );
-        playerInfo.money = playerInfo.money - 10;
-        console.log("playerInfo.money", playerInfo.money);
-        // window.prompt("Would you like to visit the shop?")
-        break;
-      }
-    }
+    if (fightOrSkip()) {
+      // if true leave fight by breaking loop
+      break;
+    };
 
     // generata random damage based on player's attack power
     var damage = randomNumber(playerInfo.attack - 3, playerInfo.attack);
